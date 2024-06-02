0787e3a108f93ef514753c016da55628cc8c2cbd
FaraazSuffla <faraazsuffla29@gmail.com>
2024-06-02 14:51:15 +0200

    Updates

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,84 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour
 {
-    public GameManager gameManager;
-    public Dropdown petDropdown;
-    public Dropdown ownerDropdown;
-
-    public Text petNameText;
-    public Text petTraitsText;
     public Text ownerNameText;
+    public Text petNameText;
     public Text ownerTraitsText;
+    public Text petTraitsText;
 
-    private Pet selectedPet;
-    private Owner selectedOwner;
+    public Owner ownerData;
+    public Pet petData;
 
-    private void Start()
+    void Start()
     {
-        UpdateDropdowns();
+        UpdateUI();
     }
 
-    public void OnPetSelected(int index)
+    void UpdateUI()
     {
-        selectedPet = gameManager.pets[index];
-        UpdatePetDetails(selectedPet);
-    }
+        ownerNameText.text = "Owner Name: " + ownerData.ownerName;
+        petNameText.text = "Pet Name: " + petData.petName;
 
-    public void OnOwnerSelected(int index)
-    {
-        selectedOwner = gameManager.owners[index];
-        UpdateOwnerDetails(selectedOwner);
-    }
-
-    public void OnMatchButtonClick()
-    {
-        // Ensure selectedPet and selectedOwner are not null
-        if (selectedPet != null && selectedOwner != null)
+        ownerTraitsText.text = "Owner Traits:\n";
+        foreach (string trait in ownerData.traits)
         {
-            gameManager.OnMatchButtonClick(selectedPet, selectedOwner);
-        }
-        else
-        {
-            Debug.LogError("Selected pet or owner is null!");
-        }
-    }
-
-    private void UpdateDropdowns()
-    {
-        if (gameManager == null)
-        {
-            Debug.LogError("GameManager is not assigned to UIManager!");
-            return;
+            ownerTraitsText.text += "- " + trait + "\n";
         }
 
-        if (gameManager.pets == null)
+        petTraitsText.text = "Pet Traits:\n";
+        foreach (string trait in petData.traits)
         {
-            Debug.LogError("GameManager.pets is null!");
-            return;
+            petTraitsText.text += "- " + trait + "\n";
         }
-
-        if (gameManager.owners == null)
-        {
-            Debug.LogError("GameManager.owners is null!");
-            return;
-        }
-
-       
-    }
-
-
-    private void UpdatePetDetails(Pet pet)
-    {
-        petNameText.text = "Name: " + pet.petName;
-        petTraitsText.text = "Traits: " + string.Join(", ", pet.traits);
-    }
-
-    private void UpdateOwnerDetails(Owner owner)
-    {
-        ownerNameText.text = "Name: " + owner.ownerName;
-        ownerTraitsText.text = "Traits: " + string.Join(", ", owner.traits);
     }
 }
